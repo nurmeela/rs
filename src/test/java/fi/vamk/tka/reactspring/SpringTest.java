7a0f7a9080e0b1e6ad151fed252aced09ba4bee1
Nurmela <e1600730@edu.vamk.fi>
2019-04-04 10:49:03 +0300

    test case added

---
+++ src/test/java/fi/vamk/tka/reactspring/SpringTest.java
@@ -0,0 +1,14 @@
+package fi.vamk.tka.reactspring;
+import org.junit.Test;
+
+public class SpringTest{
+    @Test
+    public void helloTest(){
+        System.out.println("Onko kevät koittanut");
+    }  
+
+    @Test
+    public void helloYouTest(){
+        System.out.println("Onko kesä koittanut");
+    } 
+}
